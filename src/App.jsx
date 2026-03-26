502c60fd6324e2112bb324fe8a4878d5f20d6c00
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-03-26 13:15:09 +0530

    add signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "@/pages/landingPage/LandingPage";
 import LoginPage from "./pages/auth/Login";
 import { Toaster } from "sonner";
+import SignupPage from "./pages/auth/Singup";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/ui/CustomLink.jsx
+++ src/components/ui/CustomLink.jsx
@@ -1,12 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default function CustomLink({ href, children, className = "", ...props }) {
+export default function CustomLink({ to, children, className = "", ...props }) {
   return (
-    <Link 
-      to={href} 
-      className={`items-center transition-all ${className}`}
-      {...props}
-    >
+    <Link to={to} className={`transition-all ${className}`} {...props}>
       {children}
     </Link>
   );
